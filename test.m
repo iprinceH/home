4be02a57e0f63e4dcaeed66a975584055589b9fd
iprinceH <36428816@qq.com>
2013-03-01 17:16:16 +0800

    test
    test my first objective-c program

---
+++ test.m
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+int main(int argc,char* argv[]){
+  NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+  NSLog(@"hello,my first objective-c program !");  
+[pool release];
+return 0;
+}
